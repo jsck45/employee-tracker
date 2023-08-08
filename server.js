778ae77f47bbabac644c6c24e1cdcac54946ed38
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-08-08 14:57:59 +1000

    added separate functions for db queries

--- server.js
+++ server.js
@@ -17,100 +17,69 @@ const db = mysql.createConnection({
   database: 'employee_db',
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    process.exit(1);
-  }
-  console.log('Connected to the employee_db database.');
-  // Start the server after the database connection is established
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Function to view all employees
+function viewAllEmployees() {
+  db.query('SELECT * FROM employee', (err, results) => {
+    if (err) {
+      console.error('Error fetching employees:', err);
+      return;
+    }
+    console.log(results);
   });
-});
+}
 
-const listQuestion = {
-  type: 'list',
-  name: 'action',
-  message: 'What would you like to do?',
-  choices: [
-    'View all employees',
-    'Add employee',
-    'Update employee role',
-    'View all roles',
-    'Add role',
-    'View all departments',
-    'Add department',
-  ],
-};
-
-// Handle user's choice
-function handleUserChoice(choice) {
-  switch (choice) {
-    case 'View all employees':
-      // Implement viewAllEmployees function to fetch and display all employees
-      db.query('SELECT * FROM employee', function (err, results) {
-        if (err) {
-          console.error('Error fetching employees:', err);
-          return;
-        }
-        console.log(results);
-      });
-      break;
-
-    case 'Add employee':
-      // Implement addEmployee function to add a new employee
-      inquirer
-        .prompt([
-          {
-            type: 'input',
-            name: 'first_name',
-            message: "Enter the employee's first name:",
-          },
-          {
-            type: 'input',
-            name: 'last_name',
-            message: "Enter the employee's last name:",
-          },
-          {
-            type: 'list',
-            name: 'role_id',
-            message: "What is the employee's role?",
-            choices: [
-              'Sales Lead',
-              'Salesperson',
-              'Lead Engineer',
-              'Software Engineer',
-              'Account Manager',
-              'Accountant',
-              'Legal Team Lead',
-              'Lawyer',
-            ],
-          },
-        ])
-        .then((answers) => {
-          // Insert the user input into the employee table
-          db.query(
-            'INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)',
-            [answers.first_name, answers.last_name, answers.role_id],
-            function (err, results) {
-              if (err) {
-                console.error('Error adding employee:', err);
-              } else {
-                console.log('Employee added successfully!');
-                console.log(results);
-              }
+// Function to add a new employee
+function addEmployee() {
+    inquirer .prompt([
+        {
+          type: 'input',
+          name: 'first_name',
+          message: "Enter the employee's first name:",
+        },
+        {
+          type: 'input',
+          name: 'last_name',
+          message: "Enter the employee's last name:",
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          message: "What is the employee's role?",
+          choices: [
+            'Sales Lead',
+            'Salesperson',
+            'Lead Engineer',
+            'Software Engineer',
+            'Account Manager',
+            'Accountant',
+            'Legal Team Lead',
+            'Lawyer',
+          ],
+        },
+      ])
+      .then((answers) => {
+        // Insert the user input into the employee table
+        db.query(
+          'INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)',
+          [answers.first_name, answers.last_name, answers.role_id],
+          function (err, results) {
+            if (err) {
+              console.error('Error adding employee:', err);
+            } else {
+              console.log('Employee added successfully!');
+              console.log(results);
             }
-          );
-        })
-        .catch((error) => {
-          console.error('Error occurred:', error);
-        });
-      break;
+          }
+        );
+      })
+      .catch((error) => {
+        console.error('Error occurred:', error);
+      });
+}
 
-    case 'Update employee role':
-      // Fetch the list of employees and their roles from the database
-      db.query(
+// Function to update employee role
+function updateEmployeeRole() {
+    db.query(
         'SELECT e.id, e.first_name, e.last_name, r.title FROM employee e INNER JOIN role r ON e.role_id = r.id',
         function (err, employeeResults) {
           if (err) {
@@ -172,22 +141,22 @@ function handleUserChoice(choice) {
           });
         }
       );
-      break;
+    }
 
-    case 'View all roles':
-      // Implement viewAllRoles function to fetch and display all roles
-      db.query('SELECT * FROM role', function (err, results) {
+// Function to view all roles
+function viewAllRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
         if (err) {
           console.error('Error fetching roles:', err);
           return;
         }
         console.log(results);
       });
-      break;
+    }
 
-    case 'Add role':
-      // Fetch the list of departments from the database
-      db.query('SELECT id, name FROM department', function (err, departmentResults) {
+// Function to add a new role
+function addRole() {
+    db.query('SELECT id, name FROM department', function (err, departmentResults) {
         if (err) {
           console.error('Error fetching departments:', err);
           return;
@@ -238,47 +207,78 @@ function handleUserChoice(choice) {
             console.error('Error occurred:', error);
           });
       });
-      break;
+    }
 
-    case 'View all departments':
-      // Implement viewAllDepartments function to fetch and display all departments
-      db.query('SELECT * FROM department', function (err, results) {
+// Function to view all departments
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
         if (err) {
           console.error('Error fetching departments:', err);
           return;
         }
         console.log(results);
       });
+    }
+
+// Function to add a new department
+function addDepartment() {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Enter the department's name:",
+      },
+    ])
+    .then((answers) => {
+      // Insert the new department into the database
+      db.query(
+        'INSERT INTO department (name) VALUES (?)',
+        [answers.name],
+        function (err, results) {
+          if (err) {
+            console.error('Error adding department:', err);
+          } else {
+            console.log('Department added successfully!');
+            console.log(results);
+          }
+        }
+      );
+    })
+    .catch((error) => {
+      console.error('Error occurred:', error);
+    });
+}
+
+// Handle user's choice
+function handleUserChoice(choice) {
+  switch (choice) {
+    case 'View all employees':
+      viewAllEmployees();
+      break;
+
+    case 'Add employee':
+      addEmployee();
+      break;
+
+    case 'Update employee role':
+      updateEmployeeRole();
+      break;
+
+    case 'View all roles':
+      viewAllRoles();
+      break;
+
+    case 'Add role':
+      addRole();
+      break;
+
+    case 'View all departments':
+      viewAllDepartments();
       break;
 
     case 'Add department':
-      // Implement addDepartment function to add a new department
-      inquirer
-        .prompt([
-          {
-            type: 'input',
-            name: 'name',
-            message: "Enter the department's name:",
-          },
-        ])
-        .then((answers) => {
-          // Insert the new department into the database
-          db.query(
-            'INSERT INTO department (name) VALUES (?)',
-            [answers.name],
-            function (err, results) {
-              if (err) {
-                console.error('Error adding department:', err);
-              } else {
-                console.log('Department added successfully!');
-                console.log(results);
-              }
-            }
-          );
-        })
-        .catch((error) => {
-          console.error('Error occurred:', error);
-        });
+      addDepartment();
       break;
 
     default:
@@ -289,31 +289,49 @@ function handleUserChoice(choice) {
 // Prompt the user for action using Inquirer
 function promptUser() {
   inquirer
-    .prompt([listQuestion])
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+          'View all employees',
+          'Add employee',
+          'Update employee role',
+          'View all roles',
+          'Add role',
+          'View all departments',
+          'Add department',
+        ],
+      },
+    ])
     .then((answers) => {
       handleUserChoice(answers.action);
+
+      // Prompt the user again for the next action
+      promptUser();
     })
     .catch((error) => {
       console.error('Error occurred:', error);
     });
 }
 
-// Start the prompt after the database connection is established
-db.on('connect', () => {
-  promptUser();
-});
-
-// Close the database connection when the server is closed
-process.on('SIGINT', () => {
-  db.end((err) => {
+// Start the server after the database connection is established
+db.connect((err) => {
     if (err) {
-      console.error('Error closing the database connection:', err);
-    } else {
-      console.log('Database connection closed.');
-      process.exit(0);
+      console.error('Error connecting to the database:', err);
+      process.exit(1);
     }
-  });
-});
+    console.log('Connected to the employee_db database.');
+  
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+      
+      // Prompt the user for action using Inquirer
+      promptUser();
+    });
+  });  
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
